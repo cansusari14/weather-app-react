9b23136b73c3981c26783dce94ae9d2fae3ed006
cansusari14 <63806074+cansusari14@users.noreply.github.com>
2020-11-09 19:29:32 +0100

    added unit conversion

--- src/WeatherCurrent.js
+++ src/WeatherCurrent.js
@@ -2,38 +2,84 @@ import React, { useState } from "react";
 import "./WeatherCurrent.css";
 
 export default function WeatherCurrent(props) {
+  const [unit, setUnit] = useState("celsius");
   const weatherData = props.data;
-  return (
-    <div className="weather-current">
-      <div className="row">
-        <div className="col-8">
-          <br />
-          <div className="weather-info">
-            <img
-              src={weatherData.iconUrl}
-              alt={weatherData.description}
-              className="current-icon"
-            />
-            <strong id="temperature">
-              {Math.round(weatherData.temperature)}
-            </strong>
-            <span className="units">
-              <a href=" " className="active">
-                °C
-              </a>
-              |<a href=" ">°F</a>
-            </span>
+  const fahrenheit = (weatherData.temperature * 9) / 5 + 32;
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="weather-current">
+        <div className="row">
+          <div className="col-8">
+            <br />
+            <div className="weather-info">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="current-icon"
+              />
+              <strong id="temperature">
+                {Math.round(weatherData.temperature)}
+              </strong>
+              <span className="units">
+                °C |{""}
+                <a href="/" onClick={convertToFahrenheit}>
+                  °F
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="col-4">
+            <ul className="weather-details">
+              <li className="detail-list">Wind</li>
+              <p className="wind">{weatherData.wind} km/h</p>
+              <li className="detail-list">Humidity</li>
+              <p className="humidity">{weatherData.humidity}%</p>
+            </ul>
           </div>
         </div>
-        <div className="col-4">
-          <ul className="weather-details">
-            <li className="detail-list">Wind</li>
-            <p className="wind">{weatherData.wind} km/h</p>
-            <li className="detail-list">Humidity</li>
-            <p className="humidity">{weatherData.humidity}%</p>
-          </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="weather-current">
+        <div className="row">
+          <div className="col-8">
+            <br />
+            <div className="weather-info">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="current-icon"
+              />
+              <strong id="temperature">{Math.round(fahrenheit)}</strong>
+              <span className="units">
+                <a href="/" onClick={convertToCelsius}>
+                  °C
+                </a>{" "}
+                | °F
+              </span>
+            </div>
+          </div>
+          <div className="col-4">
+            <ul className="weather-details">
+              <li className="detail-list">Wind</li>
+              <p className="wind">{weatherData.wind} km/h</p>
+              <li className="detail-list">Humidity</li>
+              <p className="humidity">{weatherData.humidity}%</p>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
